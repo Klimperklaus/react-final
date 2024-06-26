5dcb38861896abc4d124dbf921d21642f5a1458e
Klimperklaus <steven.mueller@dci-student.org>
2024-06-26 22:00:06 +0200

    better

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -27,7 +27,7 @@ export default function Home() {
   }, []);
 
   return (
-    <div className="flex flex-col gap-6 items-center">
+    <div className="flex flex-col gap-6 w-full items-center">
       <div className=" w-3/4 h-1/2 bg-white/5 border border-black rounded-2xl p-5 text-center shadow-lg shadow-white/15 mt-3">
         <h1 className="newsHeader pb-4">NEWS</h1>
         <hr />
@@ -36,13 +36,13 @@ export default function Home() {
         </p>
         <hr />
       </div>
-      <div className="flex h-1/2 gap-2 w-2/3 overflow-auto shadow-xl shadow-white/50 rounded-t-2xl">
+      <div className="flex h-1/2 w-3/4 gap-2 overflow-auto shadow-xl shadow-white/50 rounded-t-2xl">
         {galleryData &&
           galleryData.gallery.map((img) => (
             <img
               key={img.id}
               src={img.fullImageUrl}
-              className="border-8 border-white/15 border-double rounded-t-2xl"
+              className="border-4 border-white/15 border-double rounded-t-2xl"
               alt="image"
               height={300}
               width={600}
