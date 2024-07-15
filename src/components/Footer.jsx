b96ffd5676441d8cbea930a3f9a473498fafa2e5
Klimperklaus <steven.mueller@dci-student.org>
2024-07-15 16:42:39 +0200

    started responsive design. lg and md done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,10 @@
 export default function Footer() {
+  let currYear = new Date().getFullYear();
+  let createdYear = 2024;
+
   return (
     <div className="text-center border-t-4 border-zinc-400 bg-white/15 p-3">
-      &copy; Steven Müller
+      &copy; Steven Müller {createdYear === currYear ? createdYear : createdYear - currYear}
     </div>
   );
 }
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -8,7 +8,7 @@ export default function Layout() {
       <header>
         <Navbar />
       </header>
-      <main className="flex justify-center">
+      <main className="flex justify-center overflow-auto lg:h-2/3 xl:h-4/5 md:h-3/4 w-full md:top-48 lg:top-48 xl:top-32 fixed">
         <Outlet />
       </main>
       <footer>
