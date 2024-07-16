aed36965ec0085317c0cb3dadc57610bb9446010
Klimperklaus <steven.mueller@dci-student.org>
2024-07-16 15:34:55 +0200

    fixed layout in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ export default function Navbar() {
   return (
     <nav className="categories text-center text-3xl p-3 border-b-4 border-zinc-400 bg-white/15">
       <ul className="flex items-center justify-center gap-10 w-full">
-      <div className="md:flex md:flex-wrap lg:flex-none">
+      <div className="md:flex md:flex-wrap lg:flex-none gap-10">
           <li className="nav-links flex border-transparent border-b-4 border-t-4 border-l-2 border-r-2 hover:border-zinc-300/30 border-double hover:bg-white/5 rounded-3xl">
             <NavLink to="/">Home</NavLink>
           </li>
@@ -17,7 +17,7 @@ export default function Navbar() {
             TruckersMP Community Tracker
           </h1>
         </li>
-        <div className="md:flex md:flex-wrap lg:flex-none">
+        <div className="md:flex md:flex-wrap lg:flex-none gap-10">
           <li className="nav-links border-transparent border-b-4 border-t-4 border-l-2 border-r-2 hover:border-zinc-300/30 border-double hover:bg-white/5 rounded-3xl">
             <NavLink to="/players">Players</NavLink>
           </li>
